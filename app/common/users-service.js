4353f88a1ec6d765d63cd4275764a76426d2ae53
AsenTahchiyski <malkstor@gmail.com>
2016-04-18 12:00:56 +0300

    Users service updated with full functionality.

--- app/common/users-service.js
+++ app/common/users-service.js
@@ -7,9 +7,30 @@ angular
         '$q',
         'BASE_URL',
         function userAuth($http, $q, BASE_URL) {
+            function getHeader(type) {
+                var header = {};
+                switch (type) {
+                    case 'auth':
+                        return {
+                            headers: {'Authorization': 'Bearer ' + sessionStorage['authToken']}
+                        };
+                    case 'content':
+                        return {
+                            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                        };
+                    case 'both':
+                        return {
+                            headers: {
+                                'Authorization': 'Bearer ' + sessionStorage['authToken'],
+                                'Content-Type': 'application/x-www-form-urlencoded'
+                            }
+                        };
+                }
+            }
+
             function register(user) {
                 var deferred = $q.defer();
-                $http.post(BASE_URL + 'Account/Register', user)
+                $http.post(BASE_URL + 'api/Account/Register', user)
                     .then(function (success) {
                         deferred.resolve(success.data);
                     }, function (error) {
@@ -22,10 +43,73 @@ angular
             function login(user) {
                 var deferred = $q.defer();
                 var data = 'grant_type=password&username=' + user.email + '&password=' + user.password;
-                var config = {
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-                };
-                $http.post(BASE_URL + 'api/Token', data, config)
+                $http.post(BASE_URL + 'api/Token', data, getHeader('content'))
+                    .then(function (success) {
+                        deferred.resolve(success);
+                    }, function (error) {
+                        deferred.reject(error);
+                    });
+
+                return deferred.promise;
+            }
+
+            function getAll() {
+                var deferred = $q.defer();
+                $http.get(BASE_URL + 'users/', getHeader('auth'))
+                    .then(function (success) {
+                        deferred.resolve(success.data);
+                        console.log(success.data)
+                    }, function (error) {
+                        deferred.reject(error);
+                    });
+
+                return deferred.promise;
+            }
+
+            function getCurrent() {
+                var deferred = $q.defer();
+                $http.get(BASE_URL + 'users/me/', getHeader('auth'))
+                    .then(function (success) {
+                        deferred.resolve(success.data);
+                        console.log(success.data)
+                    }, function (error) {
+                        deferred.reject(error);
+                    });
+
+                return deferred.promise;
+            }
+
+            function makeAdmin(userId) {
+                getCurrent().then(function (currentUser) {
+                    if (!currentUser.isAdmin) {
+                        console.error('Only admins can do that.');
+                        return;
+                    }
+
+                    var deferred = $q.defer();
+                    var data = 'UserId=' + userId;
+                    $http.put(BASE_URL + 'users/makeadmin', data, getHeader('both'))
+                        .then(function (success) {
+                            deferred.resolve(success);
+                        }, function (error) {
+                            deferred.reject(error);
+                        });
+
+                    return deferred.promise;
+                }, function (error) {
+                    console.error(error);
+                });
+            }
+
+            function changePassword(oldPass, newPass, confirmNewPass) {
+                if (newPass != confirmNewPass) {
+                    console.error('Passwords do not match.');
+                    return;
+                }
+
+                var deferred = $q.defer();
+                var data = 'OldPassword=' + oldPass + '&NewPassword=' + newPass + '&ConfirmPassword=' + confirmNewPass;
+                $http.post(BASE_URL + 'api/Account/ChangePassword', data, getHeader('both'))
                     .then(function (success) {
                         deferred.resolve(success);
                     }, function (error) {
@@ -37,7 +121,11 @@ angular
 
             return {
                 register: register,
-                login: login
+                login: login,
+                getAll: getAll,
+                getCurrent: getCurrent,
+                changePassword: changePassword,
+                makeAdmin: makeAdmin
             }
         }
     ]);
--- app/dashboard/dashboard-controller.js
+++ app/dashboard/dashboard-controller.js
@@ -12,20 +12,37 @@ angular
         '$scope',
         '$location',
         'projectsService',
-        function ($scope, $location, projectsService) {
+        'usersService',
+        function ($scope, $location, projectsService, usersService) {
             if (!sessionStorage['authToken']) {
                 $location.path('/login');
             }
-            
+
             $scope.getProject = function (id) {
                 projectsService.getById(id);
             };
-            
+
             $scope.getAll = function () {
                 projectsService.getAll();
             };
-            
+
             $scope.getByFilter = function (filter, value, pageSize, pageNumber) {
                 projectsService.getByFilter(filter, value, pageSize, pageNumber);
+            };
+
+            $scope.getUsers = function () {
+                usersService.getAll();
+            };
+            
+            $scope.getCurrent = function () {
+                usersService.getCurrent();  
+            };
+            
+            $scope.changePass = function(oldPass, newPass, confirmNewPass) {
+                usersService.changePassword(oldPass, newPass, confirmNewPass);
+            };
+            
+            $scope.makeAdmin = function(userId) {
+                usersService.makeAdmin(userId);
             }
         }]);
